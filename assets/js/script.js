31f80bfec9ff3e6889320c48bb458906fb7819c4
Mona Lisa <cordovamonica.a@gmail.com>
2023-06-25 18:10:41 -0500

    added the questions to javascript file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,46 @@
 
-
 function countDown(num) {
     for (var i = num; i > 0; i--) {
       console.log(i);
     }
   }
+
+  var questions = [
+    { 
+      question: "Common datatypes do NOT include",
+    answers: {
+      a: "string",
+      b: "boolean",
+      c: "alerts",
+      d: "number"
+    },
+    correctAnswer: 'c'
+  },
+  {
+    question: "A variable is a named container that allows us to store data in our code",
+    answers: {
+      a: "true",
+      b: "false"
+    },
+    correctAnswer: 'a'
+  },
+  {
+    question: "JavaScript executes code from:",
+    answers: {
+      a: "markdown",
+      b: "text",
+      c: "top to bottom",
+      d: "left to right"
+    },
+    correctAnswer: 'c'
+  },
+  {
+    question: "String data must be written in:",
+    answers: {
+      a: "quotations",
+      b: "parenthesis",
+      c: "curly brackets",
+      d: "brackets"
+    },
+    correctAnswer: 'a'
+  }];
