4fdfd97fa86374e505a26e7c0ac77fd04d2aab2a
Mona Lisa <cordovamonica.a@gmail.com>
2023-06-28 22:12:41 -0500

    changed the question format

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,46 @@
 
+var questions = [
+  { 
+    question: "Common datatypes do NOT include",
+  answers: [
+    "a. string",
+    "b. boolean",
+    "c. alerts",
+    "d. number"
+  ],
+  correctAnswer: "c. alerts",
+},
+{
+  question: "A variable is a named container that allows us to store data in our code",
+  answers: [
+    "a. true",
+    "b. false",
+    "c. both",
+    "d. neither"
+  ],
+  correctAnswer: "a. true",
+},
+{
+  question: "JavaScript executes code from:",
+  answers: [
+    "a. markdown",
+    "b. text",
+    "c. top to bottom",
+    "d. left to right"
+  ],
+  correctAnswer: "c. top to bottom"
+},
+{
+  question: "String data must be written in:",
+  answers: [
+    "a. quotations",
+    "b. parenthesis",
+    "c. curly brackets",
+    "d. brackets"
+  ],
+  correctAnswer: "a. quotations"
+}];
+
 var startButtonEl = document.querySelector("#start-button");
 var timerEl = document.querySelector("countdown");
 var count = 40;
@@ -14,47 +56,7 @@ var answerD = document.querySelector("#d");
 var currentQuestion = 0;
 var penalty = 10;
 var seconds = 0;
-var questions = [
-    { 
-      question: "Common datatypes do NOT include",
-    answers: {
-      a: "string",
-      b: "boolean",
-      c: "alerts",
-      d: "number"
-    },
-    correctAnswer: 'c'
-  },
-  {
-    question: "A variable is a named container that allows us to store data in our code",
-    answers: {
-      a: "true",
-      b: "false",
-      c: "both",
-      d: "neither"
-    },
-    correctAnswer: 'a'
-  },
-  {
-    question: "JavaScript executes code from:",
-    answers: {
-      a: "markdown",
-      b: "text",
-      c: "top to bottom",
-      d: "left to right"
-    },
-    correctAnswer: 'c'
-  },
-  {
-    question: "String data must be written in:",
-    answers: {
-      a: "quotations",
-      b: "parenthesis",
-      c: "curly brackets",
-      d: "brackets"
-    },
-    correctAnswer: 'a'
-  }];
+
 
 function startTimer() {
   count = 40;
